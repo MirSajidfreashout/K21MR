788795ee8b63807254cdb5123b856ddb6b29af84
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-11-01 23:00:52 +0530

    Hackerrank DSA Day 42

---
+++ K21MR Sem 3/CSE205 DATA_STRUCTURES_AND_ALGORITHMS/CSE205_Day42_26699/Binary Search Tree  Lowest Common Ancestor.cpp
@@ -0,0 +1,20 @@
+/*The tree node has data, left child and right child
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+
+Node *lca(Node *root, int v1, int v2)
+{
+    if (root == nullptr)
+        return nullptr;
+    int data = root->data;
+    if (v1 < data && v2 < data)
+        return lca(root->left, v1, v2);
+    if (v1 > data && v2 > data)
+        return lca(root->right, v1, v2);
+    return root;
+}
